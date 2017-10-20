be2278f46604e5cfb10d4924363b57dec987339c
Miguel Bustos <Miguel Bustos>
2017-10-20 16:44:18 -0300

    Nuevo template para emails a postulantes

---
+++ erp-recruitment-folderit/templates/email-job-application.php
@@ -0,0 +1,71 @@
+<div style="font-family: 'Raleway', Arial, sans-serif; font-weight: 500; background-color: #f1f1f1;">
+  <div style="width: 600px; height: 20px; margin: 0 auto; background-color: #f1f1f1;"></div>
+  <div style="background-color: white; width: 600px; margin: 0 auto;">
+    <div style="max-width: 600px; height: auto; margin: 0 auto; text-align: center;"><a href="https://folderit.net"><img style="display: block; width: 600px;" src="https://network.folderit.net/static/header_rrhh.jpg" /></a></div>
+    <table style="width: 600px;" cellspacing="0" cellpadding="0" align="center">
+      <tbody>
+        <tr>
+          <td style="padding: 30px 25px 0 25px;"><span style="font-size: 24px; color: #3b3b3b; font-weight: bold; font-family: 'Raleway', Arial, sans-serif;">Postulación recibida - %1$s</span></td>
+        </tr>
+        <tr>
+          <td style="text-align: right; margin: 10px; width: 600px;"><label style="float: right; padding-right: 25px; font-family: 'Raleway', Arial, sans-serif;">%5$s</label></td>
+        </tr>
+        <tr>
+          <td style="padding: 10px 25px 0 25px; font-size: 14px; font-family: Franklin Gothic regular, Arial, sans-serif; font-weight: normal; line-height: 20px; text-align: justify; color: #3b3b3b;">Agradecemos tu postulación a nuestra búsqueda, y que hayas elegido a Folder IT como un posible destino en tu carrera.<br/>
+            Tu CV queda en nuestra base de datos, y lo tendremos en cuenta para las búsquedas acordes a tu perfil.<br/>
+            No obstante, nos estaremos comunicando para realizar una entrevista y así ampliar la información recibida.<br/>
+            <br/>
+            <strong>Datos de tu postulación:</strong><br/>
+            Nombre: %2$s<br/>
+            Posición: %1$s<br/>
+            Email: %3$s<br/>
+            Teléfono: %4$s<br/>
+            <br/>
+            Saludos cordiales,
+        </tr>
+      </tbody>
+    </table>
+    <table style="width: 600px; padding-bottom: 32px;" cellspacing="0" cellpadding="0" align="center">
+      <tbody>
+        <tr>
+          <td style="text-align: center; width: 96px; height: 96px; margin: 10px;"><a href="https://folderit.net"><img style="border-radius: 100 width: 64px; height: 64px; max-width: 64px; max-height: 64px;" src="https://network.folderit.net/static/logo_small.png" /></a></td>
+          <td style="vertical-align: center; width: 525px;">
+            <div style="font-size: 16px; font-family: Franklin Gothic regular, Arial, sans-serif; font-weight: bold; color: #1f1f1f;">Departamento de Recursos Humanos</div>
+            <div style="font-size: 14px; font-family: Franklin Gothic regular, Arial, sans-serif; font-weight: normal; color: #3b3b3b;">Folder IT</div></td>
+        </tr>
+      </tbody>
+    </table>
+    <div style="background-image: url('https://network.folderit.net/static/bg-black.png');">
+      <table style="width: 600px;" cellspacing="0" cellpadding="0" align="center">
+        <tbody>
+          <tr>
+            <td style="height: 100px;"><img class="aligncenter" src="https://network.folderit.net/static/bg-burgundy.png" /></td>
+            <td style="height: 100px; width: 50px;"><a href="https://facebook.com/social.folderit"><img class="aligncenter" src="https://network.folderit.net/static/logo_facebook_inverse.png" /> </a></td>
+            <td style="height: 100px; width: 50px;"><a href="https://twitter.com/folderit"><img class="aligncenter" src="https://network.folderit.net/static/logo_twitter_inverse.png" /> </a></td>
+            <td style="height: 100px; width: 50px;"><a href="https://linkedin.com/company/folderit"><img class="aligncenter" src="https://network.folderit.net/static/logo_linkedin_inverse.png" /> </a></td>
+            <td style="height: 100px; width: 50px;"><a href="https://plus.google.com/+folderit"><img class="aligncenter" src="https://network.folderit.net/static/logo_g+_inverse.png" /> </a></td>
+            <td style="height: 100px; width: 50px;"><a href="https://youtube.com/folderit"><img class="aligncenter" src="https://network.folderit.net/static/logo_youtube_inverse.png" /> </a></td>
+            <td style="height: 100px;"><img class="aligncenter" src="https://network.folderit.net/static/bg-burgundy.png" /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <table style="width: 600px;" cellspacing="0" cellpadding="0" align="center">
+      <tbody>
+        <tr>
+          <td style="text-align: center; vertical-align: center; font-family: 'Raleway', Arial, serif; font-size: 12px; color: #5c5c5c; height: 120px;">
+            <div style="padding: 15px; background-color: white;"><label>Francisco Miguens Nº 180 (Santa Fe Harbor)</label>
+              <label>9th &amp; 10th Floors, Corporate Tower</label>
+              <label>Santa Fe – 3000 – Argentina</label>
+              <label>Phone: +54 (0342) 4556006</label>
+              <label>Fax: +54 (0342) 4547979</label></div></td>
+        </tr>
+        <tr>
+          <td style="text-align: center; vertical-align: center; font-family: 'Raleway', Arial, serif; font-size: 12px; color: #5c5c5c; border-top: 1px solid #c3c3c3; height: 65px;">
+            <div style="margin: 15px; background-color: white; font-weight: normal;"><label>© Todos los derechos reservados Folder IT</label></div></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div style="height: 20px; width: 100"></div>
+</div>
