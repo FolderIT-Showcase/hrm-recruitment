ee8dc8e177ca6f26ab49fc554ea6851acac77359
Miguel Bustos <Miguel Bustos>
2017-10-27 03:15:21 -0300

    Nuevo modal para envio de emails desde postulacion

---
+++ erp-recruitment-folderit/includes/admin/views/js-templates/sendemail-template.php
@@ -0,0 +1,34 @@
+<div class="row">
+  <?php $application_id = $_GET['application_id'];?>
+  <div class="col-lg-12 form-group">
+    <?php erp_html_form_input(array(
+  'label'       => __('Subject', 'wp-erp-rec'),
+  'name'        => 'email_subject',
+  'value'       => '',
+  'type'        => 'text',
+  'id'          => 'email_subject',
+  'class'       => 'form-control',
+  'required'    => true
+)); ?>
+  </div>
+
+  <div class="col-lg-12 form-group">
+    <?php erp_html_form_input(array(
+      'label'       => __('Message', 'wp-erp-rec'),
+      'name'        => 'email_message',
+      'value'       => '',
+      'type'        => 'textarea',
+      'id'          => 'email_message',
+      'class'       => 'form-control',
+      'custom_attr' => array(
+          'rows'  => 10,
+          'media' => true,
+          'teeny' => false
+      ),
+      'required'    => true
+    )); ?>
+  </div>
+</div>
+
+<input type="hidden" value="" id="email_to" name="email_to">
+<?php wp_nonce_field('wp_erp_rec_send_email_nonce', '_sendemail_nonce'); ?>
